446822b22af820a663d14e57a0091c9d3fcf1bc2
shinzo-bzh <adrien.bernava@hotmail.com>
2024-12-10 16:25:25 +0100

    préparation pour wordpress
    Code copier sur word press

--- front-page.php
+++ front-page.php
@@ -1,7 +1,29 @@
 <?php get_header() ?>
 
-<header></header>
 
-<main></main>
 
-<?php get_footer() ?>
+<main>
+<div class="presentation">
+  <h1 classe="presentation__textpres"><?php echo get_field('presentation__textpres'); ?>  </h1>
+
+  <a href="<?php echo get_field('presentation_bouton_url'); ?>" class="presentation__bouton"> 
+  <?php echo get_field('presentation__bouton_text'); ?>
+   </a>
+
+  <div><img src="<?php echo get_field('presentation__video'); ?>" alt="vid"></div>
+
+</div>
+
+<div class="galerie">
+  <div >Réalisations</div>
+
+  <div class="galerie__images"></div>
+
+</div>
+
+</body>
+
+</html>
+</main>
+
+<?php get_footer() ?> 
